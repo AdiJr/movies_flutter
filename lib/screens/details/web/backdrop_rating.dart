fe944b9f7defe2850df86c35ed333692d3eeb385
Julia Debecka <57728399+jdebecka@users.noreply.github.com>
2020-07-22 11:01:32 +0200

    Finished details

--- lib/screens/details/web/backdrop_rating.dart
+++ lib/screens/details/web/backdrop_rating.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:movies/constants.dart';
 import 'package:movies/models/movie.dart';
-
 import '../../../models/movie.dart';
 
 class BackdropAndRating extends StatelessWidget {
@@ -19,106 +18,114 @@ class BackdropAndRating extends StatelessWidget {
     return Container(
       height: size.height / 2,
       child: Container(
-        child: Column(
+        child: Stack(
           children: <Widget>[
-            Expanded(
-              child: Container(
-                height: MediaQuery.of(context).size.height / 2,
-                decoration: BoxDecoration(
-                  image: DecorationImage(
-                    alignment: Alignment.topCenter,
-                    fit: BoxFit.cover,
-                    image: NetworkImage(imagesBaseUrl + movie.backdropPath),
-                  ),
+            Container(
+              height: size.height / 2,
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.only(bottomLeft: Radius.circular(50)),
+                image: DecorationImage(
+                  alignment: Alignment.topCenter,
+                  fit: BoxFit.cover,
+                  image: NetworkImage(imagesBaseUrl + movie.backdropPath),
                 ),
               ),
-            ),
-            Container(
-              height: MediaQuery.of(context).size.width * 0.07,
-              decoration: BoxDecoration(
-                color: Colors.white,
-                boxShadow: [
-                  BoxShadow(
-                    offset: Offset(0, 5),
-                    blurRadius: 50,
-                    color: Color(0xFF12153D).withOpacity(0.2),
+            ),// Rating Box
+            Positioned(
+              bottom: 0,
+              right: 0,
+              child: Container(
+                width: size.width * 0.9,
+                height: 100,
+                decoration: BoxDecoration(
+                  color: Colors.white,
+                  borderRadius: BorderRadius.only(
+                    bottomLeft: Radius.circular(50),
+                    topLeft: Radius.circular(50),
                   ),
-                ],
-              ),
-              child: Padding(
-                padding: const EdgeInsets.symmetric(horizontal: defaultPadding),
-                child: Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceAround,
-                  children: <Widget>[
-                    Column(
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      children: <Widget>[
-                        Icon(Icons.star,
-                          color: Colors.yellow,),
-                        //SvgPicture.asset("assets/icons/star_fill.svg"),
-                        SizedBox(height: defaultPadding / 4),
-                        RichText(
-                          text: TextSpan(
-                            style: TextStyle(color: Colors.black),
-                            children: [
-                              TextSpan(
-                                text: "${movie.voteAverage}/",
-                                style: TextStyle(
-                                    fontSize: 16, fontWeight: FontWeight.w600),
-                              ),
-                              TextSpan(
-                                text: "10\n",
-                                style: TextStyle(fontSize: 16),
-                              ),
-                            ],
-                          ),
-                        ),
-                      ],
+                  boxShadow: [
+                    BoxShadow(
+                      offset: Offset(0, 5),
+                      blurRadius: 50,
+                      color: Color(0xFF12153D).withOpacity(0.2),
                     ),
-                    // Rate this
-                    Column(
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      children: <Widget>[
-                        Icon(Icons.star,),
-                        SizedBox(height: defaultPadding / 4),
-                        Text("Rate Film",
-                            style: Theme.of(context).textTheme.bodyText2),
-                      ],
-                    ),
-                    Column(
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      children: <Widget>[
-                        Container(
-                          padding: EdgeInsets.all(6),
-                          decoration: BoxDecoration(
-                            color: Color(0xFF51CF66),
-                            borderRadius: BorderRadius.circular(2),
-                          ),
-                          child: Text(
-                            "${movie.voteCount}",
-                            style: TextStyle(
-                              fontSize: 16,
-                              color: Colors.white,
-                              fontWeight: FontWeight.w500,
-                            ),
-                          ),
-                        ),
-                        SizedBox(height: defaultPadding / 4),
-                        Text(
-                          "Metascore",
-                          style: TextStyle(
-                              fontSize: 16, fontWeight: FontWeight.w500),
-                        ),
-                        Text(
-                          "62 critic reviews",
-                          style: TextStyle(color: textLightColor),
-                        )
-                      ],
-                    )
                   ],
                 ),
+                child: Padding(
+                  padding: const EdgeInsets.symmetric(horizontal: defaultPadding),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceAround,
+                    children: <Widget>[
+                      Column(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: <Widget>[
+                          Icon(Icons.star,
+                          color: Colors.yellow,),
+                          SizedBox(height: defaultPadding / 4),
+                          RichText(
+                            text: TextSpan(
+                              style: TextStyle(color: Colors.black),
+                              children: [
+                                TextSpan(
+                                  text: "${movie.voteAverage}/",
+                                  style: TextStyle(
+                                      fontSize: 16, fontWeight: FontWeight.w600),
+                                ),
+                                TextSpan(
+                                  text: "10\n",
+                                  style: TextStyle(fontSize: 16),
+                                ),
+                              ],
+                            ),
+                          ),
+                        ],
+                      ), // Rate this
+                      Column(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: <Widget>[
+                          Icon(Icons.star),
+                          SizedBox(height: defaultPadding / 4),
+                          Text("Rate Film",
+                              style: Theme.of(context).textTheme.bodyText2),
+                        ],
+                      ),
+                      Column(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: <Widget>[
+                          Container(
+                            padding: EdgeInsets.all(6),
+                            decoration: BoxDecoration(
+                              color: Color(0xFF51CF66),
+                              borderRadius: BorderRadius.circular(2),
+                            ),
+                            child: Text(
+                              "${movie.voteCount}",
+                              style: TextStyle(
+                                fontSize: 16,
+                                color: Colors.white,
+                                fontWeight: FontWeight.w500,
+                              ),
+                            ),
+                          ),
+                          SizedBox(height: defaultPadding / 4),
+                          Text(
+                            "Metascore",
+                            style: TextStyle(
+                                fontSize: 16, fontWeight: FontWeight.w500),
+                          ),
+                          Text(
+                            "62 critic reviews",
+                            style: TextStyle(color: textLightColor),
+                          )
+                        ],
+                      )
+                    ],
+                  ),
+                ),
               ),
             ),
+// Back Button
+            SafeArea(child: BackButton(color: Colors.pink,)),
           ],
         ),
       ),
--- lib/screens/details/web/title_duration_and_fav_btn.dart
+++ lib/screens/details/web/title_duration_and_fav_btn.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
 import 'package:movies/models/movie.dart';
 import '../../../constants.dart';
 
@@ -13,20 +14,39 @@ class TitleDurationAndFabBtn extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Padding(
-      padding: EdgeInsets.all(defaultPadding),
+      padding: EdgeInsets.fromLTRB(400.0, 0.0, 400.0, 50.0),
       child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceAround,
         children: <Widget>[
           Expanded(
             child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
+              crossAxisAlignment: CrossAxisAlignment.stretch,
               children: <Widget>[
-                Center(
+                Padding(
+                  padding: EdgeInsets.fromLTRB(0.0, 0.0, 0.0, 20.0),
                   child: Text(
                     movie.title,
                     style: Theme.of(context).textTheme.headline5,
                   ),
                 ),
-                SizedBox(height: defaultPadding / 2),
+                Row(
+                  children: <Widget>[
+                    Text(
+                      '${new DateFormat.yMMMMd('en_US').format(movie.releaseDate)}',
+                      style: TextStyle(color: textLightColor),
+                    ),
+                    SizedBox(width: defaultPadding),
+                    Text(
+                      "PG-13",
+                      style: TextStyle(color: textLightColor),
+                    ),
+                    SizedBox(width: defaultPadding),
+                    Text(
+                      "2h 32min",
+                      style: TextStyle(color: textLightColor),
+                    ),
+                  ],
+                )
               ],
             ),
           ),
